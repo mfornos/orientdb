8b35ac6eb5d67769a0359110771710a89e00afec
Marc Fornós <marc.fornos@gmail.com>
2011-12-30 19:34:52 +0100

    Fixes Issue #3

--- src/play/modules/orientdb/DatabaseSource.java
+++ src/play/modules/orientdb/DatabaseSource.java
@@ -7,32 +7,35 @@ import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
 import com.orientechnologies.orient.core.db.object.ODatabaseObjectTx;
 
 public class DatabaseSource implements BeanSource {
-    private final ODatabaseDocumentTx documentDB;
-    private final ODatabaseObjectTx objectDB;
-    private final OGraphDatabase graphDB;
+	private final ODatabaseDocumentTx documentDB;
+	private final ODatabaseObjectTx objectDB;
+	private final OGraphDatabase graphDB;
 
-    public DatabaseSource(int conf) {
-        this.documentDB = isEnabled(conf, ODBPlugin.OIV_DOCUMENT_DB) ? ODB.openDocumentDB() : null;
-        this.objectDB = isEnabled(conf, ODBPlugin.OIV_OBJECT_DB) ? ODB.openObjectDB() : null;
-        this.graphDB = isEnabled(conf, ODBPlugin.OIV_GRAPH_DB) ? ODB.openGraphDB() : null;
-    }
+	public DatabaseSource(int conf) {
+		this.documentDB = isEnabled(conf, ODBPlugin.OIV_DOCUMENT_DB) ? ODB
+				.openDocumentDB() : null;
+		this.objectDB = isEnabled(conf, ODBPlugin.OIV_OBJECT_DB) ? ODB
+				.openObjectDB() : null;
+		this.graphDB = isEnabled(conf, ODBPlugin.OIV_GRAPH_DB) ? ODB
+				.openGraphDB() : null;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public <T> T getBeanOfType(Class<T> clazz) {
-        if (ODatabaseObjectTx.class.isAssignableFrom(clazz)) {
-            return (T) objectDB;
-        } else if (ODatabaseDocumentTx.class.isAssignableFrom(clazz)) {
-            return (T) documentDB;
-        } else if (OGraphDatabase.class.isAssignableFrom(clazz)) {
-            return (T) graphDB;
-        } else {
-            return null;
-        }
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getBeanOfType(Class<T> clazz) {
+		if (ODatabaseObjectTx.class.isAssignableFrom(clazz)) {
+			return (T) objectDB;
+		} else if (OGraphDatabase.class.isAssignableFrom(clazz)) {
+			return (T) graphDB;
+		} else if (ODatabaseDocumentTx.class.isAssignableFrom(clazz)) {
+			return (T) documentDB;
+		} else {
+			return null;
+		}
+	}
 
-    private boolean isEnabled(int conf, int property) {
-        return ((conf & property) == property);
-    }
+	private boolean isEnabled(int conf, int property) {
+		return ((conf & property) == property);
+	}
 
 }
